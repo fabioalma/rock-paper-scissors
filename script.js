33e6631c32157ef1bf97d3a757612d66a7c02935
Fabio <157958129+fabioalma@users.noreply.github.com>
2024-02-10 18:58:41 +0100

    docs: written script "pseudocode"
    not really pseudocode but a "diagram" of the code

---
+++ script.js
@@ -0,0 +1,8 @@
+//DEFINE variables to null
+//COMPUTE a 5 round game
+    //COMPUTE a r-p-s round (getComputerChoice, playerSelection)
+        //GET computer's choice
+        //GET player's selection
+      //RETURN round win loss or tie
+      //ADD a point to the winning player
+    //RETURN a game win loss or tie
