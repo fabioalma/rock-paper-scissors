c952aab3e0a2b019d104ead0dff581e535ec87ab
Fabio <157958129+fabioalma@users.noreply.github.com>
2024-02-10 20:50:09 +0100

    feat!: add point system
    BREAKING CHANGE: game does not work in this commit

--- script.js
+++ script.js
@@ -22,26 +22,45 @@ function getComputerChoice () {
 
 function round (playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    console.log("computer: "+computerSelection+"\nplayer: "+playerSelection)
+    //console.log("computer: "+computerSelection+"\nplayer: "+playerSelection)
     if (playerSelection === computerSelection) {
+        points(0,0);
         return(`It's a tie! Both players selected ${playerSelection}`);
     } else if (playerSelection === "rock") {
         if (computerSelection === "paper") {
+            points(1,0);
             return("You loose. Paper beats Rock");
         } else {
+            points(0,1);
             return("You win. Rock beats Scissors");
         };
     } else if (playerSelection === "paper") {
         if (computerSelection === "scissors") {
+            points(1,0);
             return("You loose. Scissors beats Paper");
         } else {
+            points(0,1);
             return("You win. Paper beats Rock");
         };
     } else {
         if (computerSelection === "rock") {
+            points(1,0);
             return("You loose. Rock beats Scissors");
         } else {
+            points(0,1);
             return("You win. Scissors beats Paper");
         };
     };
+}
+
+function points (computerPoints,playerPoints) {
+    if (computerPoints === 0 && playerPoints === 0) {
+        return(0)
+    } else if (computerPoints === 1) {
+        computerPoints++
+        return(computerPoints)
+    } else {
+        playerPoints++
+        return(playerPoints)
+    }
 }
