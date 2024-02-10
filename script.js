f7d04bda56911d5f71b6b6b6ff95f15e288763ac
Fabio <157958129+fabioalma@users.noreply.github.com>
2024-02-10 20:25:54 +0100

    feat: Can play one round

--- script.js
+++ script.js
@@ -7,12 +7,12 @@
       //ADD a point to the winning player
     //RETURN a game win loss or tie
 
-let computerSelection;
-let playerSelection;
+let playerChoice = prompt("Choose: Rock, Paper or Scissors","");
+let computerChoice = getComputerChoice();
+console.log(round(playerChoice, computerChoice));
 
 function getComputerChoice () {
     let randNum = Math.floor(Math.random()*3);
-    console.log(randNum);
     return (
         (randNum == 0) ? "rock" : 
         (randNum == 1) ? "paper" :
@@ -20,4 +20,28 @@ function getComputerChoice () {
     );
 }
 
-console.log(getComputerChoice());
+function round (playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    console.log("computer: "+computerSelection+"\nplayer: "+playerSelection)
+    if (playerSelection === computerSelection) {
+        return(`It's a tie! Both players selected ${playerSelection}`);
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return("You loose. Paper beats Rock");
+        } else {
+            return("You win. Rock beats Scissors");
+        };
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            return("You loose. Scissors beats Paper");
+        } else {
+            return("You win. Paper beats Rock");
+        };
+    } else {
+        if (computerSelection === "rock") {
+            return("You loose. Rock beats Scissors");
+        } else {
+            return("You win. Scissors beats Paper");
+        };
+    };
+}
