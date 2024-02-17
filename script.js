b9a5cf99fee903f5998897880e6b71e1fd1acbd6
Fabio <157958129+fabioalma@users.noreply.github.com>
2024-02-17 19:34:32 +0100

    feat: how to play instructions have been added

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ let computerChoice;
 
 const startBtn = document.querySelector('#start-btn');
 const startBtnBg = document.querySelector('.play');
-const mainPage = document.querySelector('.main-page')
+const mainPage = document.querySelector('.main-page');
+const body = document.querySelector('body');
+const fillerRight = document.querySelector('.filler.right');
+const fillerLeft = document.querySelector('.filler.left');
+
 let isGameOn = false;
 startBtn.addEventListener('click', ()=>{
     startBtnBg.remove();
@@ -19,8 +23,61 @@ startBtn.addEventListener('click', ()=>{
 
 const score = document.querySelector('#points');
 const textOut = document.querySelector('.text-out');
-const playH1 = document.querySelector('#play-h1');
+const playH1 = document.querySelector('.play-h1');
 const playP = document.querySelector('#play-p');
+const howTo = document.querySelector('#how-to');
+const howToLink = document.querySelector('.how-to-link');
+const howToBtn = document.querySelector('#how-to-btn');
+
+howToLink.addEventListener('click', (event) => {
+    const div = document.createElement('div');
+    const para = document.createElement('p');
+    const img = document.createElement('img');
+
+    event.preventDefault();
+    para.textContent = 'How to Play?'
+    para.setAttribute('class', 'play-p')
+    div.appendChild(para);
+    img.setAttribute('src', 'img/instructions-context.png');
+    img.setAttribute('id','instructions-context-img')
+    div.appendChild(img);
+    div.setAttribute('class', 'instructions');
+    startBtnBg.appendChild(div);
+    div.addEventListener('mouseleave', () => {
+        startBtnBg.addEventListener('click', () => {
+            div.remove();
+        });
+    });
+    
+});
+
+
+howToBtn.addEventListener('click', () => {
+    const div = document.createElement('div');
+    const para = document.createElement('p');
+    const img = document.createElement('img');
+    const closeBtn = document.createElement('p');
+
+    para.textContent = 'Forgot How to Play?';
+    para.setAttribute('class', 'play-p')
+    div.appendChild(para);
+    img.setAttribute('src', 'img/instructions.png');
+    img.setAttribute('id','instructions-img');
+    div.appendChild(img);
+    //closeBtn.textContent = 'Close';
+    closeBtn.setAttribute('class', 'close-btn');
+    div.setAttribute('class', 'instructions');
+    fillerLeft.appendChild(div);
+    mainPage.appendChild(closeBtn);
+
+    closeBtn.addEventListener('click', () => {
+        div.remove();
+        closeBtn.remove();
+    });
+});
+
+
+
 
 const options = document.querySelector('.options');
 options.addEventListener('click', (event) => {
@@ -49,9 +106,15 @@ options.addEventListener('click', (event) => {
                     playerChoice = 'Spock';
                     result = playRound('spock',computerChoice);
                     break;
+                default:
+                    playerChoice = null;
+                    result = playRound(playerChoice,computerChoice);
+                    break;
             };
 
-            if (result === "tie") {
+            if (result === '') {
+                textOut.textContent = 'You didn\'t press a button! Try again';
+            } else if (result === "tie") {
                 textOut.textContent = `It's a tie! Both players selected ${playerChoice}`
             } else if (result) {
                 playerPoints++;
@@ -59,21 +122,23 @@ options.addEventListener('click', (event) => {
             } else if (!result) {
                 computerPoints++;
                 textOut.textContent = `You loose this round! ${computerChoice.at(0).toUpperCase().concat(computerChoice.slice(1))} beats ${playerChoice}`;
-            } else {
-                textOut.textContent = `How did you even get this result??`;
             };
+
+            result = ''
 
             score.textContent = playerPoints+" - "+computerPoints;
 
             if (computerPoints < 5 && playerPoints == 5 ) {
                 playH1.textContent = 'Congratulations! You win!';
                 playP.textContent = `Final score: ${playerPoints}-${computerPoints}`;
+                howTo.textContent = '';
                 startBtn.textContent = 'Play Again';
                 isGameOn = false;
                 mainPage.appendChild(startBtnBg);
             } else if (computerPoints == 5 && playerPoints < 5 ) {
-                playH1.textContent = `Bad luck! You'll win next time...`;
+                playH1.textContent = `You lost! You'll win next time...`;
                 playP.textContent = `Final score: ${playerPoints}-${computerPoints}`;
+                howTo.textContent = '';
                 startBtn.textContent = 'Play Again';
                 isGameOn = false;
                 mainPage.appendChild(startBtnBg);
@@ -94,7 +159,9 @@ function getComputerChoice () {
 }
 
 function playRound (playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
+    if (playerSelection === null) {
+        return '';
+    } else if (playerSelection === computerSelection) {
         return("tie");
     } else if (
         (playerSelection === "rock" && computerSelection === "scissors") || 
